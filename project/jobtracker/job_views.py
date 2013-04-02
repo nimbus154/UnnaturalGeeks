81df1feb9d581090652f240d3aca79b807326af1
nimbus154 <nimbus154@gmail.com>
2013-04-01 17:52:16 -0700

    removed unused job_views

--- project/jobtracker/job_views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def list(request):
-    return HttpResponse('job list')
-
-def detail(request, job_id):
-    return HttpResponse('details for job id %s' % job_id)
